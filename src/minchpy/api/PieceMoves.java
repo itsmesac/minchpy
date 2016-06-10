56a552d0853eaa913dbd9231f1b28aa0ffbc5828
itsmesac <sachindraac@gmail.com>
2016-06-09 23:22:41 -0700

    more changes

--- src/minchpy/api/PieceMoves.java
+++ src/minchpy/api/PieceMoves.java
@@ -4,5 +4,7 @@ import java.util.List;
 import minchpy.entity.Move;
 
 public interface PieceMoves {
+	
+	/* Method to generate all possible legal moves of a piece */
 	List<Move> getAllMoves();
 }
--- src/minchpy/entity/ChessMove.java
+++ src/minchpy/entity/ChessMove.java
@@ -2,4 +2,6 @@ package minchpy.entity;
 
 public class ChessMove extends Move {
 	//TODO: implement ChessMove class
+	public Square src;
+	public Square dst;
 }
