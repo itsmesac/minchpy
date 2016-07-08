6d954e7d3ec83a8162479eebcbec6d7eb24f05d3
itsmesac <sachindraac@gmail.com>
2016-07-07 22:28:16 -0700

    Main function to start the game

---
+++ src/minchpy/main/MinchPy.java
@@ -0,0 +1,15 @@
+package minchpy.main;
+
+import minchpy.entity.*;
+
+public class MinchPy {
+    Board cb;
+    
+	public MinchPy() {
+		super();
+		this.cb = new ChessBoard();
+	}
+
+	public static void main(String[] args) {
+	}
+}
