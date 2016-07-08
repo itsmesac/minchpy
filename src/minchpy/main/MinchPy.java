15af7b0cc5353b51e429d34dd1bd949d52a0ba1a
Sharvani Adiga <sharvani.adiga@gmail.com>
2016-07-07 22:32:12 -0700

    code to take user input

--- src/minchpy/main/MinchPy.java
+++ src/minchpy/main/MinchPy.java
@@ -1,6 +1,9 @@
 package minchpy.main;
 
-import minchpy.entity.*;
+import java.util.Scanner;
+
+import minchpy.entity.Board;
+import minchpy.entity.ChessBoard;
 
 public class MinchPy {
     Board cb;
@@ -11,5 +14,9 @@ public class MinchPy {
 	}
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String userInput1 = in.next();
+		String userInput2 = in.next();
+		System.out.println(userInput1 + " " + userInput2);
 	}
 }
