abc3107f9b81f042a815439e4bbb4cfb5903886b
itsmesac <sachindraac@gmail.com>
2016-07-12 00:08:16 -0700

    whitespace cleanup

--- src/minchpy/entity/Evaluation.java
+++ src/minchpy/entity/Evaluation.java
@@ -1,23 +1,23 @@
 package minchpy.entity;
 
 public class Evaluation {
-	double evalValue;
-	
-	public Evaluation() {
-		super();
-	}
+    double evalValue;
+    
+    public Evaluation() {
+        super();
+    }
 
-	public Evaluation(double evalValue) {
-		super();
-		this.evalValue = evalValue;
-	}
+    public Evaluation(double evalValue) {
+        super();
+        this.evalValue = evalValue;
+    }
 
-	public double getEvalValue() {
-		return evalValue;
-	}
+    public double getEvalValue() {
+        return evalValue;
+    }
 
-	public void setEvalValue(double evalValue) {
-		this.evalValue = evalValue;
-	}
-	
+    public void setEvalValue(double evalValue) {
+        this.evalValue = evalValue;
+    }
+    
 }
